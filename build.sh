2a6ee3ddacdc78e6c159a202bd7f63bb2680d5ae
Courtney <crg123@virginia.edu>
2024-12-02 00:19:22 -0500

    Starting prose documentation for exoneration data cleaning

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ fi
 # printf 'Would you like to push to GU domains? (y/n)? '
 # read answer
 # if [ "$answer" != "${answer#[Yy]}" ] ;then 
-#     rsync -alvr --delete _site/* jfhgeorg@gtown.reclaimhosting.com:/home/jfhgeorg/public_html/dsan-5000/
+#     rsync -alvr --delete _site/* crggeorg@15.204.146.110:/home/crggeorg/public_html/portfolio/projects/dsan-5000
 # else
 #     echo NOT PUSHING TO GU DOMAINS!
 # fi
