5660d92fbfbd97c8ac25f449982d5f438b9cf88e
Courtney <crg123@georgetown.edu>
2024-12-13 05:15:15 -0500

    added 5100 to portfolio

--- xtra/multiclass-portfolio-website/build.sh
+++ xtra/multiclass-portfolio-website/build.sh
@@ -18,7 +18,7 @@ for i in $(find _site -type d); do chmod 755 $i; done
 printf 'Would you like to push to GU domains: (y/n)? '
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then 
-    rsync -alvr --delete _site/* crggeorg@15.204.146.110:/home/crggeorg/public_html/portfolio/
+    rsync -alvr --delete _site/* crggeorg@15.204.146.110:/home/crggeorg/public_html
 else
     echo NOT PUSHING TO GU DOMAINS!
 fi
